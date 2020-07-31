cf3a419a93531d65b2f4587fcf1b411c3a93e4ba
Bhupinderpal Singh Kaur <bp.singh@hotmail.com>
2020-07-31 16:41:23 +0200

    added new fields to drivers table

--- controllers/driver.controller.js
+++ controllers/driver.controller.js
@@ -1,20 +1,104 @@
 const mysql = require("../models/db");
 const Driver = require("../models/driver.model");
+const { check, validationResult } = require("express-validator");
 
 exports.getOne = (req, res, next) => {
   res.json("Sending one");
 };
 
-exports.create = (req, res, next) => {
-  const { name, email, mobile } = req.body;
-  const driver = { name, email, mobile };
-  Driver.create(driver)
-    .then((result) => {
-      console.log(result);
-      return res.json(result);
-    })
-    .catch((err) => {
-      console.log("Error " + err);
-      res.status(400).status(err.msg);
-    });
-};
+exports.create = [
+  check("name")
+    .isLength({ min: 3 })
+    .withMessage("Name must be minimum 3 characters long")
+    .trim()
+    .escape(),
+  check("email_id")
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Enter a valid email"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be minimum 6 characters long")
+    .trim()
+    .escape(),
+  check("license")
+    .exists()
+    .withMessage("Please provide the license number")
+    .trim()
+    .escape(),
+  check("address")
+    .isLength({ min: 6 })
+    .withMessage("Address must be minimum 6 characters long")
+    .trim()
+    .escape(),
+  check("pincode")
+    .isLength({ min: 5 })
+    .withMessage("Pincode must be minimum 5 characters long")
+    .isNumeric()
+    .withMessage("Enter a valid pincode")
+    .trim()
+    .escape(),
+  check("city")
+    .isLength({ min: 3 })
+    .withMessage("City must be minimum 3 characters long")
+    .trim()
+    .escape(),
+  check("created_on")
+    .isISO8601()
+    .toDate()
+    .withMessage("created_on must be a valid date"),
+  /*  check("status").custom((value) => {
+    const DRIVER_STATUS = ["active", "inactive", "deleted"];
+    if (DRIVER_STATUS.indexOf(value.toLowerCase()) === -1) {
+      return Promise.reject(
+        "Drivers status must be one of Active, Inactive or Deleted"
+      );
+      Promise.resolve();
+    }
+  }), */
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+    const {
+      name,
+      email_id,
+      mobile,
+      password,
+      license,
+      regno,
+      address,
+      pincode,
+      city,
+      created_on,
+      status,
+    } = req.body;
+    const driver = {
+      name,
+      email_id,
+      mobile,
+      password,
+      license,
+      status,
+      created_on,
+    };
+
+    //Optional parameters
+    if (regno) driver.regno = regno;
+    if (address) driver.address = address;
+    if (pincode) driver.pincode = pincode;
+    if (city) driver.city = city;
+
+    console.log("Created object " + driver);
+    Driver.create(driver)
+      .then((result) => {
+        console.log(result);
+        return res.json(result);
+      })
+      .catch((err) => {
+        console.log("Error " + err);
+        res.status(400).status(err.msg);
+      });
+  },
+];
--- models/driver.model.js
+++ models/driver.model.js
@@ -4,14 +4,27 @@ const Driver = function (driver) {
   this.name = driver.name;
   this.email = driver.email;
   this.mobile = driver.mobile;
+  this.password = driver.password;
+  this.license = driver.license;
+  this.regno = driver.regno;
+  this.address = driver.address;
+  this.pincode = driver.pincode;
+  this.city = driver.city;
+  this.status = driver.status;
+  this.created_on = driver.created_on;
 };
 
 Driver.create = (newDriver) => {
   return new Promise((resolve, reject) => {
     db.query("INSERT INTO drivers SET ?", newDriver, (err, result) => {
+      // Database error
       if (err) {
         reject("Error while saving driver");
       }
+
+      // Handle Wrong field value errors
+
+      //Record created
       resolve({
         msg: "Driver created",
         driver: {
