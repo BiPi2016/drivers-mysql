5698caed91c5c8892e3889518fb401ed517f04ed
Bhupinderpal Singh Kaur <bp.singh@hotmail.com>
2020-09-30 14:05:41 +0200

    Used cypto-js using aes

--- util/password.js
+++ util/password.js
@@ -1,6 +1,8 @@
-const bcrypt = require("bcryptjs");
+//const bcrypt = require("bcryptjs");
+const CryptoJS = require("crypto-js");
 
-exports.createHash = async (enteredPassword) => {
+//Using bcryptjs
+/* exports.createHash = async (enteredPassword) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashed = await bcrypt.hash(enteredPassword, salt);
@@ -8,9 +10,9 @@ exports.createHash = async (enteredPassword) => {
   } catch (err) {
     throw new Error(err);
   }
-};
+}; */
 
-exports.comparePasswords = async (enteredPassword, registeredPassword) => {
+/* exports.comparePasswords = async (enteredPassword, registeredPassword) => {
   console.log(enteredPassword, registeredPassword);
   try {
     return bcrypt.compare(enteredPassword, registeredPassword);
@@ -18,4 +20,26 @@ exports.comparePasswords = async (enteredPassword, registeredPassword) => {
     console.error("Error while comparing passwords " + error);
     throw new Error("Server error, could not validate password");
   }
+}; */
+
+//Using crypto-js
+exports.createHash = (enteredPassword) => {
+  // Encrypt
+  let ciphertext = CryptoJS.AES.encrypt(
+    enteredPassword,
+    process.env.CRYPTO_SECRET_KEY
+  ).toString();
+  console.log("hashed password" + ciphertext);
+  return ciphertext;
+};
+
+exports.comparePasswords = async (enteredPassword, registeredPassword) => {
+  console.log(enteredPassword, registeredPassword);
+  const bytes = CryptoJS.AES.decrypt(
+    registeredPassword,
+    process.env.CRYPTO_SECRET_KEY
+  );
+  const originalText = bytes.toString(CryptoJS.enc.Utf8);
+  console.log(originalText);
+  return enteredPassword === originalText;
 };
